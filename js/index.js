131e1f0d72ab4d4fc2ed731aaff0ce8031996a64
ryanslh <ryanslh5@gmail.com>
2015-08-20 13:40:15 +0100

    Added character animations

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
-$('body').on('click', function(event) {
+$('#atm1').on('click', function(event) {
     event.preventDefault();
     alert('body');
 });
+
+$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $('#character').stop().animate({
+            left: '-=60'
+        }); //left arrow key
+        break;
+    case 38:
+        $('#character').stop().animate({
+            top: '-=60'
+        }); //up arrow key
+        break;
+    case 39:
+        $('#character').stop().animate({
+            left: '+=60'
+        }); //right arrow key
+        break;
+    case 40:
+        $('#character').stop().animate({
+            top: '+=60'
+        }); //bottom arrow key
+        break;
+    }
+})
